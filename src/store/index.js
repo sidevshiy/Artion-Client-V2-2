426f9d154fe7e92dde219d6d4a2de081dc80f128
sidevshiy <evgeni_lyax7@mail.ru>
2021-11-26 20:32:17 +0100

    Put local storage on client side only

--- src/store/index.js
+++ src/store/index.js
@@ -19,34 +19,36 @@ Object.keys(_modules).forEach(_moduleName => {
     modules[_moduleName] = _modules[_moduleName].module;
 });
 
-const vuexLocalStorage = new VuexPersist({
-    // The key to store the state on in the storage provider.
-    key: `vuex-${appConfig.code}`,
-    storage: window.localStorage,
-    // Function that passes the state and returns the state with only the Objects you want to store.
-    reducer: _state => {
-        const store = {};
-
-        Object.keys(_modules).forEach(_moduleName => {
-            const storeInLocalStorage = _modules[_moduleName].storeInLocalStorage;
-            const state = _state[_moduleName];
-
-            if (state && storeInLocalStorage && storeInLocalStorage.length > 0) {
-                store[_moduleName] = {};
-
-                storeInLocalStorage.forEach(_key => {
-                    if (_key in state) {
-                        store[_moduleName][_key] = state[_key];
-                    }
-                });
-            }
-        });
-
-        return store;
-    },
-});
-
-plugins.push(vuexLocalStorage.plugin);
+if(process.isClient) {
+    const vuexLocalStorage = new VuexPersist({
+        // The key to store the state on in the storage provider.
+        key: `vuex-${appConfig.code}`,
+        storage: window.localStorage,
+        // Function that passes the state and returns the state with only the Objects you want to store.
+        reducer: _state => {
+            const store = {};
+    
+            Object.keys(_modules).forEach(_moduleName => {
+                const storeInLocalStorage = _modules[_moduleName].storeInLocalStorage;
+                const state = _state[_moduleName];
+    
+                if (state && storeInLocalStorage && storeInLocalStorage.length > 0) {
+                    store[_moduleName] = {};
+    
+                    storeInLocalStorage.forEach(_key => {
+                        if (_key in state) {
+                            store[_moduleName][_key] = state[_key];
+                        }
+                    });
+                }
+            });
+    
+            return store;
+        },
+    });
+    
+    plugins.push(vuexLocalStorage.plugin);
+}
 
 Vue.use(Vuex);
 
